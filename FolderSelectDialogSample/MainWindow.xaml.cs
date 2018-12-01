d9d461167f8ed61a79d30f8f457635fd354c36bc
강배준 <kbjjun0707@naver.com>
2018-12-02 06:52:22 +0900

    초기 커밋.

---
+++ FolderSelectDialogSample/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace FolderSelectDialogSample {
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e) {
+            FolderSelectDialog fsd = new FolderSelectDialog();
+            fsd.Multiselect = true;
+            if (fsd.ShowDialog()) {
+                string[] dirs = fsd.FileNames;
+                lstview.ItemsSource = dirs;
+            }
+        }
+    }
+}
